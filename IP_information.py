2d1692c62d45fd04b22788262deb9758eb7a2750
Buddhika Kuruppu <buddhikakuruppu@hotmail.com>
2022-10-13 12:55:56 +1100

    initial commit v1.0

---
+++ IP_information.py
@@ -0,0 +1,33 @@
+import requests
+import socket
+
+hostname = input("Please enter website address:\n")
+
+#def get_ip():
+    #response = requests.get('https://api64.ipify.org?format=json').json()
+    #return response["ip"]
+    #response = {"IP": "103.102.220.5"}
+    #json_object = json.loads(json_data)
+    #json_formatted_str = json.dumps(json_object, indent=2)
+    #return response
+def get_location():
+    ip_address = socket.gethostbyname(hostname)
+    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+    location_data = {
+        "ip": ip_address,
+        "version": response.get("version"),
+        "postal": response.get("postal"),
+        "city": response.get("city"),
+        "region": response.get("region"),
+        "country": response.get("country_name"),
+        "latitude": response.get("latitude"),
+        "longitude": response.get("longitude"),
+        "timezone": response.get("timezone"),
+        "utc_offset": response.get("utc_offset"),
+        "country_calling_code": response.get("country_calling_code"),
+        "asn": response.get("asn"),
+        "org": response.get("org")  
+    }
+    return location_data
+
+print(get_location())
